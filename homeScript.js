eba20093d58b8d02d1cb8e7dbba048277f9e9254
ViHoff <vice.hof@hotmail.com>
2025-03-21 16:49:33 -0300

    back da conversao pronto e paginas arrumadas

--- homeScript.js
+++ homeScript.js
@@ -1,4 +1,7 @@
+console.log("homeScript.js iniciado")
+
 fetchData();
+
 
 document.addEventListener("DOMContentLoaded", function () { //Para copiar os dados dos seletores
     const selectOrigem = document.getElementById("moedaOrigem");
@@ -8,22 +11,40 @@ document.addEventListener("DOMContentLoaded", function () { //Para copiar os dad
 });
 
 
-async function fetchData() //feito para debug
+async function fetchData() 
 {
+    const valorOrigem = document.getElementById("valorOrigem"); //input origem
+    const valorConvertido = document.getElementById("valorConvertido"); //input destino
+    
+    const valor = parseFloat(valorOrigem.value); //variavel contendo o valor que deseja converter
+    const from =  moedaOrigem.value; //variavel contendo o codigo da moeda origem
+    const to = moedaDestino.value; //variavel contendo o codigo da moeda de destino
+    
+
+
     try
     {
-        const response = await fetch(`https://economia.awesomeapi.com.br/json/last/${}-${}`);
+        var apikey = from + "-" + to; //para colar na url da api
+        var key = from  + to; //para chamar a bid
+        const response = await fetch(`https://economia.awesomeapi.com.br/json/last/${apikey}`);
 
         if(!response.ok)
         {
             throw new Error("Could not fetch");
         }
         const data = await response.json();
-        console.log(data);
+        console.log(data);//debug
+        console.log(data[key].bid);//debug
 
+        var taxa = parseFloat(data[key].bid);
+        valorConvertido.value = (valor * taxa).toFixed(2);
     }
     catch(error)
     {
-        console.error(error);
+        console.error("Erro ao buscar taxa de Cambio",error);
     }
 }
+
+valorOrigem.addEventListener("input", fetchData);
+moedaOrigem.addEventListener("change", fetchData);
+moedaDestino.addEventListener("change", fetchData);
